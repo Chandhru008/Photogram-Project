99eef0da2e612c58a9d9cb959c454ad3f8044621
Chandhru <chadhrukusalavan1234@gmail.com>
2024-09-01 08:16:52 +0000

    php photogram

---
+++ libs/load.php
@@ -0,0 +1,6 @@
+<?php
+function load_template($name){
+    print("including ".__DIR__."../_templates/$name.php");         //displayes the content what is getting included
+
+    include $_SERVER['DOCUMENT_ROOT']."/app/_templates/$name.php";    //__DIR__. current dir la irundhu po 
+}
