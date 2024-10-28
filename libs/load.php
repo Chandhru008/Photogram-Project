1f3279e6608d6dfe99cf2e03facc42e4f30f55cd
Chandhru <chadhrukusalavan1234@gmail.com>
2024-10-28 06:47:45 +0000

    Sessions added

--- libs/load.php
+++ libs/load.php
@@ -1,9 +1,10 @@
 
 <?php
-
+include_once 'includes/Session.class.php';
 include_once 'includes/Mic.class.php';
 include_once 'includes/Database.class.php';
 include_once 'includes/User.class.php';
+Session::start();
 
 function load_template($name){
    // print("including ".__DIR__."../_templates/$name.php");         //displayes the content what is getting included
